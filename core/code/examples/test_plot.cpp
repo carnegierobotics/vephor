608e288f00c888cbea0c34c8638f5dc5ceb52aef
Ethan J. Musser <emusser@carnegierobotics.com>
2025-05-21 19:26:07 -0400

    Add/update scatter plot examples

--- core/code/examples/test_plot.cpp
+++ core/code/examples/test_plot.cpp
@@ -1,97 +1,146 @@
 /**
- * Copyright 2023
+ * Copyright 2023 - 2025
  * Carnegie Robotics, LLC
  * 4501 Hatfield Street, Pittsburgh, PA 15201
  * https://www.carnegierobotics.com
  *
  * This code is provided under the terms of the Master Services Agreement (the Agreement).
  * This code constitutes CRL Background Intellectual Property, as defined in the Agreement.
-**/
+ **/
 
 #include "vephor_ext.h"
 
 using namespace vephor;
+
+
+vector<float> linspace(const float start, const float end, const int num)
+{
+    vector<float> result;
+
+    if (num <= 0)
+    {
+        return result;
+    }
+
+    if (num == 1)
+    {
+        result.push_back(start);
+        return result;
+    }
+
+    const auto delta = (end - start) / (num - 1);
+    for (int i = 0; i < num; ++i)
+    {
+        result.push_back(start + i * delta);
+    }
+
+    return result;
+}
+
 
 int main()
 {
     Plot plt;
 
     plt.title("Squared");
+    plt.xlabel("X");
+    plt.ylabel("Y");
 
-    plt.xlabel("X Axis");
-    plt.ylabel("Y Axis");
+    //
+    // Data
+    //
+
+    const auto xs = linspace(-2.0, 2.0, 101);
+
+    vector<float> ys_linear;
+    vector<float> ys_sqrt;
+    vector<float> ys_square;
+    vector<float> ys_cube;
+    vector<float> ys_sin;
+    for (const auto x : xs)
+    {
+        ys_linear.emplace_back(x);
+        ys_sqrt.emplace_back(std::sqrt(x));
+        ys_square.emplace_back(x * x);
+        ys_cube.emplace_back(x * x * x);
+        ys_sin.emplace_back(std::sin(x));
+    }
+
+    //
+    // Line plots
+    //
+
+    plt.plot(xs, ys_linear, {.label = "x thick line", .thickness_in_screen_perc = 1.0});
+    plt.plot(xs, ys_linear, {.label = "x thin line"});
+
+    //
+    // Scatter plots
+    //
+
+    plt.scatter(xs, ys_cube, {.label = "x^3 scatter", .color = {0.0F, 0.0F, 1.0F}, .marker = PlotScatterMarker::PLUS});
 
     {
-        vector<float> x;
-        vector<float> y;
-        for (int i = 0; i < 100; i++) {
-            x.push_back(i);
-            y.push_back(i * i);
+        vector<float> xs_half;
+        vector<float> ys_half;
+        for (int i = 0; i < xs.size(); ++i)
+        {
+            if (i % 2 == 0)
+            {
+                xs_half.emplace_back(xs[i]);
+                ys_half.emplace_back(ys_cube[i]);
+            }
         }
-        PlotLineOptions opts;
-        opts.label = "Thick Line";
-        opts.thickness_in_screen_perc = 2.5;
-        plt.plot(x, y, opts);
+
+        plt.scatter(xs_half,
+                    ys_half,
+                    {.label = "x^3 half-step scatter", .color = {0.0F, 1.0F, 0.0F}, .marker = PlotScatterMarker::STAR});
+    }
+
+    //
+    // Colored scatter plots
+    //
+
+    {
+        //
+        // Plot a square root function with random colors assigned to each point
+        //
+
+        std::random_device rd;
+        std::mt19937 rng(rd());
+        std::uniform_real_distribution<float> c_dist(0, 1);
+
+        vector<Color> c;
+        for (int i = 0; i < xs.size(); ++i)
+        {
+            c.emplace_back(c_dist(rng), c_dist(rng), c_dist(rng));
+        }
+
+        plt.scatter(xs, ys_sqrt, c, {.label = "sqrt(x) with random colors", .marker = PlotScatterMarker::CIRCLE});
     }
 
     {
-        vector<float> x;
-        vector<float> y;
-        for (int i = 0; i < 100; i++) {
-            x.push_back(i);
-            y.push_back(i * i);
-        }
-        PlotLineOptions opts;
-        opts.label = "Less Thick Line";
-        opts.thickness_in_screen_perc = 1.0;
-        plt.plot(x, y, opts);
+        //
+        // Plot a square function with colors assigned via a y-value color map
+        //
+
+        plt.scatter_colormap(xs,
+                             ys_square,
+                             PlotScatterOptions{.label = "x^2 colored by y-value",
+                                                .marker = PlotScatterMarker::DIAMOND,
+                                                .colormap_style = ColorMap::Style::VIRIDIS});
     }
 
     {
-        vector<float> x;
-        vector<float> y;
-        for (int i = 0; i < 100; i++) {
-            x.push_back(i);
-            y.push_back(i * i);
-        }
-        PlotLineOptions opts;
-        opts.label = "Thin Line";
-        plt.plot(x, y, opts);
-    }
+        //
+        // Plot a sine function with colors assigned via an x-value color map
+        //
 
-    {
-        vector<float> x;
-        vector<float> y;
-        for (int i = 0; i < 100; i++) {
-            x.push_back(static_cast<float>(i));
-            y.push_back(static_cast<float>(i * i));
-        }
-
-        plt.scatter(x, y, {.label = "Scatter", .color = {0.0F, 0.0F, 1.0F}, .marker = PlotScatterMarker::PLUS});
-    }
-
-    {
-        vector<float> x;
-        vector<float> y;
-        for (int i = 0; i < 100; i += 2) {
-            x.push_back(static_cast<float>(i));
-            y.push_back(static_cast<float>(i * i));
-        }
-
-        plt.scatter(x, y,
-                    {.label = "Double-Step Scatter", .color = {0.0F, 1.0F, 0.0F}, .marker = PlotScatterMarker::STAR});
-    }
-
-    {
-        vector<float> x;
-        vector<float> y;
-        for (int i = 1; i < 99; i += 4) {
-            x.push_back(static_cast<float>(i));
-            y.push_back(static_cast<float>(i * i));
-        }
-
-        plt.scatter(x, y,
-                    {.label = "Arrow Scatter", .color = {0.0F, 1.0F, 0.0F}, .marker = PlotScatterMarker::ARROW_RIGHT});
+        plt.scatter_colormap(xs,
+                             ys_sin,
+                             xs,
+                             PlotScatterOptions{.label = "sin(x) colored by x-value",
+                                                .marker = PlotScatterMarker::STAR,
+                                                .colormap_style = ColorMap::Style::JET});
     }
 
     {
---
+++ core/code/examples/test_scatter.cpp
@@ -0,0 +1,164 @@
+/**
+ * Copyright 2025
+ * Carnegie Robotics, LLC
+ * 4501 Hatfield Street, Pittsburgh, PA 15201
+ * https://www.carnegierobotics.com
+ *
+ * This code is provided under the terms of the Master Services Agreement (the Agreement).
+ * This code constitutes CRL Background Intellectual Property, as defined in the Agreement.
+ **/
+
+#include "vephor_ext.h"
+
+using namespace vephor;
+
+
+struct Iris
+{
+    float sepal_length;
+    float sepal_width;
+    float petal_length;
+    float petal_width;
+    string species;
+};
+
+//
+// The Fisher Iris data set
+//
+// [1] https://en.wikipedia.org/wiki/Iris_flower_data_set
+//
+const vector<Iris> iris_dataset{
+    {5.1, 3.5, 1.4, 0.2, "setosa"},     {4.9, 3.0, 1.4, 0.2, "setosa"},     {4.7, 3.2, 1.3, 0.2, "setosa"},
+    {4.6, 3.1, 1.5, 0.2, "setosa"},     {5.0, 3.6, 1.4, 0.2, "setosa"},     {5.4, 3.9, 1.7, 0.4, "setosa"},
+    {4.6, 3.4, 1.4, 0.3, "setosa"},     {5.0, 3.4, 1.5, 0.2, "setosa"},     {4.4, 2.9, 1.4, 0.2, "setosa"},
+    {4.9, 3.1, 1.5, 0.1, "setosa"},     {5.4, 3.7, 1.5, 0.2, "setosa"},     {4.8, 3.4, 1.6, 0.2, "setosa"},
+    {4.8, 3.0, 1.4, 0.1, "setosa"},     {4.3, 3.0, 1.1, 0.1, "setosa"},     {5.8, 4.0, 1.2, 0.2, "setosa"},
+    {5.7, 4.4, 1.5, 0.4, "setosa"},     {5.4, 3.9, 1.3, 0.4, "setosa"},     {5.1, 3.5, 1.4, 0.3, "setosa"},
+    {5.7, 3.8, 1.7, 0.3, "setosa"},     {5.1, 3.8, 1.5, 0.3, "setosa"},     {5.4, 3.4, 1.7, 0.2, "setosa"},
+    {5.1, 3.7, 1.5, 0.4, "setosa"},     {4.6, 3.6, 1.0, 0.2, "setosa"},     {5.1, 3.3, 1.7, 0.5, "setosa"},
+    {4.8, 3.4, 1.9, 0.2, "setosa"},     {5.0, 3.0, 1.6, 0.2, "setosa"},     {5.0, 3.4, 1.6, 0.4, "setosa"},
+    {5.2, 3.5, 1.5, 0.2, "setosa"},     {5.2, 3.4, 1.4, 0.2, "setosa"},     {4.7, 3.2, 1.6, 0.2, "setosa"},
+    {4.8, 3.1, 1.6, 0.2, "setosa"},     {5.4, 3.4, 1.5, 0.4, "setosa"},     {5.2, 4.1, 1.5, 0.1, "setosa"},
+    {5.5, 4.2, 1.4, 0.2, "setosa"},     {4.9, 3.1, 1.5, 0.1, "setosa"},     {5.0, 3.2, 1.2, 0.2, "setosa"},
+    {5.5, 3.5, 1.3, 0.2, "setosa"},     {4.9, 3.1, 1.5, 0.1, "setosa"},     {4.4, 3.0, 1.3, 0.2, "setosa"},
+    {5.1, 3.4, 1.5, 0.2, "setosa"},     {5.0, 3.5, 1.3, 0.3, "setosa"},     {4.5, 2.3, 1.3, 0.3, "setosa"},
+    {4.4, 3.2, 1.3, 0.2, "setosa"},     {5.0, 3.5, 1.6, 0.6, "setosa"},     {5.1, 3.8, 1.9, 0.4, "setosa"},
+    {4.8, 3.0, 1.4, 0.3, "setosa"},     {5.1, 3.8, 1.6, 0.2, "setosa"},     {4.6, 3.2, 1.4, 0.2, "setosa"},
+    {5.3, 3.7, 1.5, 0.2, "setosa"},     {5.0, 3.3, 1.4, 0.2, "setosa"},     {7.0, 3.2, 4.7, 1.4, "versicolor"},
+    {6.4, 3.2, 4.5, 1.5, "versicolor"}, {6.9, 3.1, 4.9, 1.5, "versicolor"}, {5.5, 2.3, 4.0, 1.3, "versicolor"},
+    {6.5, 2.8, 4.6, 1.5, "versicolor"}, {5.7, 2.8, 4.5, 1.3, "versicolor"}, {6.3, 3.3, 4.7, 1.6, "versicolor"},
+    {4.9, 2.4, 3.3, 1.0, "versicolor"}, {6.6, 2.9, 4.6, 1.3, "versicolor"}, {5.2, 2.7, 3.9, 1.4, "versicolor"},
+    {5.0, 2.0, 3.5, 1.0, "versicolor"}, {5.9, 3.0, 4.2, 1.5, "versicolor"}, {6.0, 2.2, 4.0, 1.0, "versicolor"},
+    {6.1, 2.9, 4.7, 1.4, "versicolor"}, {5.6, 2.9, 3.6, 1.3, "versicolor"}, {6.7, 3.1, 4.4, 1.4, "versicolor"},
+    {5.6, 3.0, 4.5, 1.5, "versicolor"}, {5.8, 2.7, 4.1, 1.0, "versicolor"}, {6.2, 2.2, 4.5, 1.5, "versicolor"},
+    {5.6, 2.5, 3.9, 1.1, "versicolor"}, {5.9, 3.2, 4.8, 1.8, "versicolor"}, {6.1, 2.8, 4.0, 1.3, "versicolor"},
+    {6.3, 2.5, 4.9, 1.5, "versicolor"}, {6.1, 2.8, 4.7, 1.2, "versicolor"}, {6.4, 2.9, 4.3, 1.3, "versicolor"},
+    {6.6, 3.0, 4.4, 1.4, "versicolor"}, {6.8, 2.8, 4.8, 1.4, "versicolor"}, {6.7, 3.0, 5.0, 1.7, "versicolor"},
+    {6.0, 2.9, 4.5, 1.5, "versicolor"}, {5.7, 2.6, 3.5, 1.0, "versicolor"}, {5.5, 2.4, 3.8, 1.1, "versicolor"},
+    {5.5, 2.4, 3.7, 1.0, "versicolor"}, {5.8, 2.7, 3.9, 1.2, "versicolor"}, {6.0, 2.7, 5.1, 1.6, "versicolor"},
+    {5.4, 3.0, 4.5, 1.5, "versicolor"}, {6.0, 3.4, 4.5, 1.6, "versicolor"}, {6.7, 3.1, 4.7, 1.5, "versicolor"},
+    {6.3, 2.3, 4.4, 1.3, "versicolor"}, {5.6, 3.0, 4.1, 1.3, "versicolor"}, {5.5, 2.5, 4.0, 1.3, "versicolor"},
+    {5.5, 2.6, 4.4, 1.2, "versicolor"}, {6.1, 3.0, 4.6, 1.4, "versicolor"}, {5.8, 2.6, 4.0, 1.2, "versicolor"},
+    {5.0, 2.3, 3.3, 1.0, "versicolor"}, {5.6, 2.7, 4.2, 1.3, "versicolor"}, {5.7, 3.0, 4.2, 1.2, "versicolor"},
+    {5.7, 2.9, 4.2, 1.3, "versicolor"}, {6.2, 2.9, 4.3, 1.3, "versicolor"}, {5.1, 2.5, 3.0, 1.1, "versicolor"},
+    {5.7, 2.8, 4.1, 1.3, "versicolor"}, {6.3, 3.3, 6.0, 2.5, "virginica"},  {5.8, 2.7, 5.1, 1.9, "virginica"},
+    {7.1, 3.0, 5.9, 2.1, "virginica"},  {6.3, 2.9, 5.6, 1.8, "virginica"},  {6.5, 3.0, 5.8, 2.2, "virginica"},
+    {7.6, 3.0, 6.6, 2.1, "virginica"},  {4.9, 2.5, 4.5, 1.7, "virginica"},  {7.3, 2.9, 6.3, 1.8, "virginica"},
+    {6.7, 2.5, 5.8, 1.8, "virginica"},  {7.2, 3.6, 6.1, 2.5, "virginica"},  {6.5, 3.2, 5.1, 2.0, "virginica"},
+    {6.4, 2.7, 5.3, 1.9, "virginica"},  {6.8, 3.0, 5.5, 2.1, "virginica"},  {5.7, 2.5, 5.0, 2.0, "virginica"},
+    {5.8, 2.8, 5.1, 2.4, "virginica"},  {6.4, 3.2, 5.3, 2.3, "virginica"},  {6.5, 3.0, 5.5, 1.8, "virginica"},
+    {7.7, 3.8, 6.7, 2.2, "virginica"},  {7.7, 2.6, 6.9, 2.3, "virginica"},  {6.0, 2.2, 5.0, 1.5, "virginica"},
+    {6.9, 3.2, 5.7, 2.3, "virginica"},  {5.6, 2.8, 4.9, 2.0, "virginica"},  {7.7, 2.8, 6.7, 2.0, "virginica"},
+    {6.3, 2.7, 4.9, 1.8, "virginica"},  {6.7, 3.3, 5.7, 2.1, "virginica"},  {7.2, 3.2, 6.0, 1.8, "virginica"},
+    {6.2, 2.8, 4.8, 1.8, "virginica"},  {6.1, 3.0, 4.9, 1.8, "virginica"},  {6.4, 2.8, 5.6, 2.1, "virginica"},
+    {7.2, 3.0, 5.8, 1.6, "virginica"},  {7.4, 2.8, 6.1, 1.9, "virginica"},  {7.9, 3.8, 6.4, 2.0, "virginica"},
+    {6.4, 2.8, 5.6, 2.2, "virginica"},  {6.3, 2.8, 5.1, 1.5, "virginica"},  {6.1, 2.6, 5.6, 1.4, "virginica"},
+    {7.7, 3.0, 6.1, 2.3, "virginica"},  {6.3, 3.4, 5.6, 2.4, "virginica"},  {6.4, 3.1, 5.5, 1.8, "virginica"},
+    {6.0, 3.0, 4.8, 1.8, "virginica"},  {6.9, 3.1, 5.4, 2.1, "virginica"},  {6.7, 3.1, 5.6, 2.4, "virginica"},
+    {6.9, 3.1, 5.1, 2.3, "virginica"},  {5.8, 2.7, 5.1, 1.9, "virginica"},  {6.8, 3.2, 5.9, 2.3, "virginica"},
+    {6.7, 3.3, 5.7, 2.5, "virginica"},  {6.7, 3.0, 5.2, 2.3, "virginica"},  {6.3, 2.5, 5.0, 1.9, "virginica"},
+    {6.5, 3.0, 5.2, 2.0, "virginica"},  {6.2, 3.4, 5.4, 2.3, "virginica"},  {5.9, 3.0, 5.1, 1.8, "virginica"},
+};
+
+void plotIrisesSize()
+{
+    //
+    // Aggregate the data by species
+    //
+
+    unordered_map<string, vector<Iris>> irises_by_species;
+    for (const auto& iris : iris_dataset)
+    {
+        irises_by_species[iris.species].emplace_back(iris);
+    }
+
+    //
+    // Plot dimensions with a separate legend entry for each species
+    //
+
+    Plot plt;
+    plt.title("Iris Dataset");
+    plt.xlabel("Sepal Width");
+    plt.ylabel("Sepal Length");
+
+    for (const auto& [species, irises] : irises_by_species)
+    {
+        vector<float> sepal_lengths;
+        vector<float> sepal_widths;
+        vector<float> petal_lengths;
+        for (const auto& iris : irises)
+        {
+            sepal_lengths.emplace_back(iris.sepal_length);
+            sepal_widths.emplace_back(iris.sepal_width);
+            petal_lengths.emplace_back(iris.petal_length);
+        }
+
+        plt.scatter_cs(sepal_widths,
+                       sepal_lengths,
+                       vector<float>{},
+                       petal_lengths,
+                       PlotScatterOptions{.label = species, .marker = PlotScatterMarker::CIRCLE});
+    }
+
+    plt.show();
+}
+
+void plotIrisesColormap()
+{
+    //
+    // Collect relevant data
+    //
+
+    vector<float> sepal_lengths;
+    vector<float> sepal_widths;
+    vector<float> petal_lengths;
+    for (const auto& iris : iris_dataset)
+    {
+        sepal_lengths.emplace_back(iris.sepal_length);
+        sepal_widths.emplace_back(iris.sepal_width);
+        petal_lengths.emplace_back(iris.petal_length);
+    }
+
+    //
+    // Plot dimensions with color assigned based on petal length
+    //
+
+    Plot plt;
+    plt.title("Iris Dataset");
+    plt.xlabel("Sepal Width");
+    plt.ylabel("Sepal Length");
+
+    plt.scatter_cs(sepal_widths,
+                   sepal_lengths,
+                   petal_lengths,
+                   vector<float>{},
+                   PlotScatterOptions{.label = "Petal Length", .marker = PlotScatterMarker::CIRCLE});
+
+    plt.show();
+}
+
+int main()
+{
+    plotIrisesSize();
+    plotIrisesColormap();
+}
