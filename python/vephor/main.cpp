1af7fb1ef7c5ba9f371b7c86ae5ee4db63eb7417
Steve Landers <slanders@carnegierobotics.com>
2026-07-06 02:13:31 -0400

    More corrections for particle color logic

--- python/vephor/main.cpp
+++ python/vephor/main.cpp
@@ -811,7 +811,7 @@ PYBIND11_MODULE(_core, m) {
 				MatX colors_mat = colors_obj.cast<MatX>();
 				// If it casts successfully and looks like a per-vertex color matrix:
 				// It should be empty, and should not have singular dimensions.
-				if (colors_mat.size() == 0 || (colors_mat.rows() > 1 && colors_mat.rows() > 1)) {
+				if (colors_mat.size() == 0 || (colors_mat.rows() > 1 && colors_mat.cols() > 1)) {
 					return make_shared<Particle>(verts, colors_mat, sizes);
 				}
 			} catch (const py::cast_error&) {
